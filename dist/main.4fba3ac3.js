959ed4510d7bac51b0291ac9ddeb5241da5b03f5
Ranze <1066467243@qq.com>
2022-01-12 18:00:59 +0800

    修改旋转

--- dist/main.4fba3ac3.js
+++ dist/main.4fba3ac3.js
@@ -132,7 +132,7 @@ var step = function step() {
             n += 1;
             step();
         }
-    }, 50); //用50比较好
+    }, 0); //用50比较好
 };
 
 step();
@@ -165,7 +165,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '60448' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53591' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
